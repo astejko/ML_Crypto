53652ae95a2c8b8869fd76a490b596e64860723e
Andrey <astejko@outlook.com>
2021-08-26 18:52:42 -0400

    upkeep

---
+++ BTC_price/ML_RSI.py
@@ -0,0 +1,34 @@
+import csv
+import urllib2
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+import matplotlib.ticker as ticker
+from matplotlib.figure import Figure
+import matplotlib.gridspec as gridspec
+import matplotlib as mpl
+import matplotlib.style as mplstyle
+import matplotlib.dates as mdates
+from matplotlib import cm
+
+from API.API_exchange import calcRSI, plotRSI, preRSI
+
+
+numDays =30
+
+#plotRSI(numDays)
+
+
+
+pRSI = preRSI(numDays)
+
+
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
+
+ax.contourf(pRSI, levels = np.linspace(-0.5,0.5,20),cmap=cm.bwr)
+
+plt.show()
